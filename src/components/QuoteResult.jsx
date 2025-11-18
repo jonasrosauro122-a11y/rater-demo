943adf690c0e71988c54156d48984240fac9e67d
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-18 14:38:56 -0500

    Update QuoteResult.jsx

--- src/components/QuoteResult.jsx
+++ src/components/QuoteResult.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+import CarrierCard from "./CarrierCard";
 
+export default function QuoteResult({ quotes }) {
+  if (!quotes || quotes.length === 0) return null;
+
+  return (
+    <ul>
+      {quotes.map((quote, index) => (
+        <CarrierCard key={index} carrier={quote.carrier} premium={quote.premium} />
+      ))}
+    </ul>
+  );
+}
