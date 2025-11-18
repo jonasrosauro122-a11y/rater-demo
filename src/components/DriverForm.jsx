a26a7fe6b2979f6af9fad21b05ec7116bc18c74d
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-18 15:22:52 -0500

    Update DriverForm.jsx

--- src/components/DriverForm.jsx
+++ src/components/DriverForm.jsx
@@ -1,1 +1,33 @@
+import React from "react";
 
+export default function DriverForm({ driver, index, handleDriverChange, removeDriver }) {
+  return (
+    <div className="driver-form">
+      <h4>Driver {index + 1}</h4>
+      <input
+        placeholder="Full Name"
+        value={driver.name}
+        onChange={(e) => handleDriverChange(index, "name", e.target.value)}
+        required
+      />
+      <input
+        type="date"
+        placeholder="DOB"
+        value={driver.dob}
+        onChange={(e) => handleDriverChange(index, "dob", e.target.value)}
+        required
+      />
+      <input
+        placeholder="Driver License Number"
+        value={driver.licenseNumber}
+        onChange={(e) => handleDriverChange(index, "licenseNumber", e.target.value)}
+      />
+      <input
+        placeholder="License State"
+        value={driver.licenseState}
+        onChange={(e) => handleDriverChange(index, "licenseState", e.target.value)}
+      />
+      <button type="button" onClick={() => removeDriver(index)}>Remove Driver</button>
+    </div>
+  );
+}
