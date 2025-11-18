46426ff7bae61382a70b9194bafaab6387ce4042
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-18 15:41:52 -0500

    Delete netlify/functions directory

--- netlify/functions/quoteAuto.js
+++
@@ -1,7 +0,0 @@
-const { calculateQuotes } = require("../../src/utils/rating");
-
-exports.handler = async (event) => {
-  const data = JSON.parse(event.body);
-  const quotes = calculateQuotes("auto", data);
-  return { statusCode: 200, body: JSON.stringify(quotes) };
-};
