6b6b94378fe06b6e40610e6be22747237a683205
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-18 14:39:18 -0500

    Update carriers.js

--- src/data/carriers.js
+++ src/data/carriers.js
@@ -1,1 +1,5 @@
-
+export const carriers = [
+  { name: "Carrier 1", baseRate: { auto: 1200, home: 800 } },
+  { name: "Carrier 2", baseRate: { auto: 1150, home: 850 } },
+  { name: "Carrier 3", baseRate: { auto: 1250, home: 780 } },
+];
