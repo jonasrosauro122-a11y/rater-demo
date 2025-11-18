0c5008cc58e8de91158962f581214745528e3bb2
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-18 15:24:05 -0500

    Update quoteAuto.js

--- netlify/functions/quoteAuto.js
+++ netlify/functions/quoteAuto.js
@@ -1,7 +1,7 @@
-import { calculateQuotes } from "../../src/utils/rating.js";
+const { calculateQuotes } = require("../../src/utils/rating");
 
-export async function handler(event) {
+exports.handler = async (event) => {
   const data = JSON.parse(event.body);
   const quotes = calculateQuotes("auto", data);
   return { statusCode: 200, body: JSON.stringify(quotes) };
-}
+};
