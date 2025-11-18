7599a91bdb95c7cf5a5985e517c9ceb41216bd9a
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-18 14:39:42 -0500

    Update rating.js

--- src/utils/rating.js
+++ src/utils/rating.js
@@ -1,1 +1,19 @@
+import { carriers } from "../data/carriers";
 
+export function calculateQuotes(insuranceType, details) {
+  return carriers.map(carrier => {
+    let premium = carrier.baseRate[insuranceType];
+
+    if (insuranceType === "auto") {
+      if (details.age < 25) premium *= 1.2;
+      if (details.accidents > 0) premium *= 1 + 0.1 * details.accidents;
+    } else if (insuranceType === "home") {
+      if (details.homeValue > 500000) premium *= 1.2;
+    }
+
+    return {
+      carrier: carrier.name,
+      premium: premium.toFixed(2),
+    };
+  });
+}
