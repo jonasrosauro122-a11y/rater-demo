98226cca05088d1184dcdd396af0ff5238ddbe6f
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-18 14:40:18 -0500

    Update quoteHome.js

--- netlify/functions/quoteHome.js
+++ netlify/functions/quoteHome.js
@@ -1,1 +1,12 @@
+import { calculateQuotes } from "../../src/utils/rating.js";
 
+export async function handler(event) {
+  const { homeValue } = JSON.parse(event.body);
+
+  const quotes = calculateQuotes("home", { homeValue });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(quotes),
+  };
+}
